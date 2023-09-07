fe99ead68a6882caae5badabbeef91fa12f76924
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-07 14:23:17 -0700

    updatng

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-int main(void){
+     /**
+      * main - Ntrey point
+      *
+      * Description : |*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); }	
+	return (0);
+ }	
