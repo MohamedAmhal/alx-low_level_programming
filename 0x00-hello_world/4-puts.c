5d0e3bb0a81c8f5256421c9a41c675834097b8ff
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-07 14:38:57 -0700

    jffn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-     /**
-      * main - Etrey point
-      *
-      * Description : A C program that prints with put function
-      *
-      * Return: Always 0 (Success) 
-     */
+
+/**
+ * main - this is the main put function
+ * Return: 0
+ */
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+		return (0);
+}
+
