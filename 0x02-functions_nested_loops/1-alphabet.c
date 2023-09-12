bdd8641b762bc7e0d38a85f3c6e106ecd03ee927
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-12 12:01:04 -0700

    dndkj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * the fuction -alphabet print the alphabet in lowercase !!
- *
- * retun 0 in success program and 1 in errer program
- */
+* the fuction -alphabet print the alphabet in lowercase !!
+*
+* print-aiphabet is fcuntion print the alphabet of english
+*
+* retun 0 in success program and 1 in errer program
+*/
 
 void print_alphabet(void)
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
