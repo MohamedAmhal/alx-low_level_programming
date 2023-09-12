5151e06e0f1cdca2c9643bfc66bfd27502c236d6
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-12 10:39:11 -0700

    hoho

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /* .C Main For ALX Students Provided with Betty style */
 /**
-* main - Entry point
-* Description: Provided Description
-* Return: 0 (Success)
-*/
+ * main - Entry point
+ * Description: Provided Description
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-return (0);
+	return (0);
 }
