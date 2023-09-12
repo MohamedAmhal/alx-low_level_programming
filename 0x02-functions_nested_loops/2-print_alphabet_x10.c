e93a67339be98fd788a893ad2a756cfcdc9abe6f
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-12 12:36:38 -0700

    vdhsv

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print the alphabest 10 times
+*
+* Return: the function return 0 if that succuss
+*/
+
+void print_alphabet_x10(void)
+{
+
+char u;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (c	= 'a'; u <= 'z'; u++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
