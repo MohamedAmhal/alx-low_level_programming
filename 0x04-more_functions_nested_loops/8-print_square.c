6d42e3ed6e715c7999776bd9396a0f7e6db7705e
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-14 15:32:26 -0700

    go

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - this function print the square
+ *
+ * @size:this varibale is the number of line in the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
