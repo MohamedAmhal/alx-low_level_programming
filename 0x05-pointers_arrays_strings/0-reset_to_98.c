7fd551e429a6fda26dc5fee20de6a68a33faac83
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-18 09:42:56 -0700

    the first file in this directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function rest the number n in a value const 98
+ *
+ * @n:this is a pointer pointe in a value n in the main function
+ *
+ * return:return any things because this is a void function
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
