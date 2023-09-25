bfaabdbc89638cefe3df4599c6add0795e822c05
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-25 03:51:05 -0700

    updating

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - this function search the first occurence of a unsigned char
+ * @s:the first parameter
+ * @c:the seconde parameter
+ * Return: the value char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int j = 0;
+
+	while (s[j])
+		j++;
+	for (i = 0; i < j; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+	}
+	return (&s[i]);
+}
