ca2ce1fab0a4f7136c98e610efed791c88a3c70a
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-25 08:29:22 -0700

    updating

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - this function print the lenght of a string in onther string
+ * @s:the first parameter
+ * @accept:the seconde parameter
+ * Return: void
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int t = 0;
+	int n = 0;
+	unsigned int l = 0;
+
+	while (accept[t])
+		t++;
+	while (s[n])
+		n++;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < t; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				l++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (l);
+		}
+	}
+	return (l);
+}
