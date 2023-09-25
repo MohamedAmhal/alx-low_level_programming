a93ea10b3f0fcc7cda62eed305e39f17bf4d7f74
MohamedAmhal <mohammedamhal349@gmail.com>
2023-09-25 10:24:24 -0700

    updating

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int indice(char *haystack, char *needle);
+int _putchar(char x);
+void print_chessboard(char (*a)[8]);
 
 
 #endif
