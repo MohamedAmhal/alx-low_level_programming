c034ededc33267728aca579edffd8a1009ade1a0
MohamedAmhal <mohammedamhal349@gmail.com>
2023-10-02 07:23:10 -0700

    the first file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * this programme print the name followed by a new line
+ * main - this programme print the name followed by a new line
+ * @argc: the varible 1
+ * @argv: the seconde one
  * Return: always 0 (success)
  */
 
 int main(int argc, char **argv)
 {
 	while (argc--)
-		printf("%s\n",*argv++);
+		printf("%s\n", *argv++);
 	return (0);
 }
