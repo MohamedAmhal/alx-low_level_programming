f8655af03022bae23488d853e3eee54c18309923
MohamedAmhal <mohammedamhal349@gmail.com>
2023-10-10 14:41:48 -0700

    updating

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  struct dog - structure
+ *  @name:the first vr
+ *  @age: the second vr
+ *  @owner: te third vr
+ *  Description: long
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
