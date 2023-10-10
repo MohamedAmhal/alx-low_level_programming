be7d5c066ae18e1c5916ee33e42bf9d456557039
MohamedAmhal <mohammedamhal349@gmail.com>
2023-10-10 16:10:19 -0700

    updating

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,8 @@ void print_dog(struct dog *d);
   * dog_t - typedef for struct dog
   **/
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
