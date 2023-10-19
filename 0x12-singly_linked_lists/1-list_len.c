8088d7763d4b4daad10761567044411bcb264919
mohammed <mohamenfffjrnffj@gmail.com>
2023-10-19 14:05:53 -0700

    updating

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include <stddef.h>
 
 /**
- * list_len - this is a function that return a lenght of a list
- * @h: this is the poiter
- * Return: return the legnth
- */
+* list_len - this is a function that return a lenght of a list
+* @h: this is the poiter
+* Return: return the legnth
+*/
 
 size_t list_len(const list_t *h)
 {
-	size_t l = 0;
+size_t l = 0;
 
-	while (h != NULL)
-	{
-		n++;
-		h = h->next;
-	}
-	return (n);
+while (h != NULL)
+{
+l++;
+h = h->next;
+}
+return (l);
 }
