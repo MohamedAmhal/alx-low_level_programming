402be06d653dd12c0e13fe05093f623cb502f0b3
mohammed <mohamenfffjrnffj@gmail.com>
2023-10-19 15:24:47 -0700

    updating

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - this is a function print before the main function
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
