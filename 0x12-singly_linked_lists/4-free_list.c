26e9dee1709d23f00922d92f923f72fc4880178e
mohammed <mohamenfffjrnffj@gmail.com>
2023-10-19 15:17:40 -0700

    updating

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_list - this is a function that free memory
+ * @head: the header
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *a;
+
+	while (head)
+	{
+		a = head->next;
+		free(head->str);
+		free(head);
+		head = a;
+	}
