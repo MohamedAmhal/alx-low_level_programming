352124ae6366f7882463ed505b6d699efbfb9b44
mohammed <mohamenfffjrnffj@gmail.com>
2023-10-26 11:22:26 -0700

    updating

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,5 +4,7 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
+int _putchar(char c);
+
 #endif
 
