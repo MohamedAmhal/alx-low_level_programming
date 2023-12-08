8af61780b46d3984c9309efd1813126c24c913b6
MohamedAmhal <mohammedamhal349@gmail.com>
2023-12-08 14:49:22 -0800

    updating

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - the nodes of all element
+ * @head: the head
+ * @index: indexing
+ * Return: null
+ **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *tmp;
+
+	size = 0;
+	if (head == NULL)
+	return (NULL);
+
+	tmp = head;
+	while (tmp)
+	{
+	if (index == size)
+	return (tmp);
+	size++;
+	tmp = tmp->next;
+	}
+	return (NULL);
+}
